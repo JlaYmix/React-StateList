436ba9af252647047e4d6679c3474202081eead3
Maxim <meizutelefonovich@gmail.com>
2024-12-05 00:11:30 +0200

    ListOfUsers

--- src/components/UserList-L/UserList.jsx
+++ src/components/UserList-L/UserList.jsx
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import UserCard from '../UserCard';
 
 class UserList extends Component {
-    render() {
-        const users = [
+    state = { 
+        users: [
             { id: 1, name: "Іван", gender: "male", age: 25 },
             { id: 2, name: "Олена", gender: "female", age: 30 },
             { id: 3, name: "Петро", gender: "male", age: 35 },
-        ];
-
-        const UserListCard = users.map((user) => (
+        ],
+    } 
+    render() {
+        const { users } = this.state;
+        const userListCard = users.map((user) => (
             <UserCard 
                 key={user.id}
                 id={user.id}
@@ -17,11 +19,11 @@ class UserList extends Component {
                 gender={user.gender}
             />
         ));
-
+        
         return (
-        <div>
-            {UserListCard}
-        </div>
+            <div>
+                {userListCard}
+            </div>
         )
     }
 }
