a0943d3444d25545f5dfb01fd2fa26fa23712202
Maxim <meizutelefonovich@gmail.com>
2024-12-05 00:11:20 +0200

    own-render-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import UserCard from './components/UserCard';
 import UserList from './components/UserList-L/UserList';
 import React from 'react';
 
@@ -7,16 +6,6 @@ class App extends React.Component {
     return (
       <div>
         <h1>Мій додаток</h1>
-
-        {/* Відображення окремих карток користувачів */}
-        <div> 
-          <h2>Окремі картки</h2>
-          <UserCard id="1" name="" gender="male" />
-          <UserCard id="2" name="Антон" gender="male" />
-          <UserCard id="3" name="" gender="female" />
-          <UserCard id="4" name="Гість" gender="other" />
-        </div>
-
         {/* Відображення списку користувачів */}
         <div>
           <h2>Список користувачів</h2>
